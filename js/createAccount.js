7a8f3a1e23c558b259665bb8cb321e793bc7f40e
squizzles <robertschroder@vlan-2633-10-19-20-171.student.wireless.sydney.edu.au>
2014-05-26 14:31:26 +1000

    Info1003 Webpage
    Step Up

---
+++ js/createAccount.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$("input#submit").click(function(){
+
+		//Check the passwords Match
+
+		//See if User already exists
+
+		//See if usedname is taken
+
+		//send form to dataBase
+		alert("Congradulations you've created a Step Up account. Have a look around.");
+		
+	});
+});
